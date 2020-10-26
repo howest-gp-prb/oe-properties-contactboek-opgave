87268bc5c3eb035999ce869bd5760ee7b5a8e581
dmouriss <dieter.mourisse@howest.be>
2020-10-26 11:41:39 +0100

    Update Contactbook to .NET Core

--- Prb.Properties.ContactBook/App.xaml.cs
+++ Prb.Properties.ContactBook/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ContactBook.WPF
+namespace Prb.Properties.ContactBook
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Prb.Properties.ContactBook/MainWindow.xaml.cs
+++ Prb.Properties.ContactBook/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ContactBook.WPF
+namespace Prb.Properties.ContactBook
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
